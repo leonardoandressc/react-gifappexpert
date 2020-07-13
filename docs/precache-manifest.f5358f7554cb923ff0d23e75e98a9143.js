84618971bb3dd5072a6690171092555558dd3672
Leonardo Sánchez <leonardo.andressc@gmail.com>
2020-07-12 23:20:31 -0500

    first comit

---
+++ docs/precache-manifest.f5358f7554cb923ff0d23e75e98a9143.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4647d8dbcfc87384adeaf43798584678",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0cb6c4a901e93e79239e",
+    "url": "/static/css/main.71ee5322.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cb6c4a901e93e79239e",
+    "url": "/static/js/main.48307de5.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
